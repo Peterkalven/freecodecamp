b15b241700db5578e753483999d7d5649110dd01
Peterkalven <peterkalven@foxmail.com>
2024-12-03 15:02:32 +0800

    Update index.js

--- Request Header Parser Microservice/index.js
+++ Request Header Parser Microservice/index.js
@@ -1,48 +1,31 @@
-const express = require("express");
+require('dotenv').config();
+var express = require('express');
+var app = express();
 
-const app = express();
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-function getInfo(req) {
-  const ua = req.headers["user-agent"];
-  return {
-    ipaddress: req.headers["x-forwarded-for"] || req.ip,
-    language: req.acceptsLanguages()[0],
-    software: ua.slice(ua.indexOf("(") + 1, ua.indexOf(")"))
-  };
-}
+app.use(express.static('public'));
 
-// home page
-app.get("/", (req, res) => {
-  const clientData = getInfo(req);
-
-  const page = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-      <title>Free Code Camp Request Header Parser Microservice</title>
-      <style>body {font: 1em "Open Sans"; margin: 40px}</style>
-    </head>
-    <body>
-      <h2>Free Code Camp :: Request Header Parser Microservice</h2>
-      <hr>
-      <ul>
-        <li>Your IP Address is <em>${clientData.ipaddress}</em></li>
-        <li>Your language is <em>${clientData.language}</em></li>
-        <li>Your software is <em>${clientData.software}</em></li>
-      </ul>
-      <hr>
-      <a href="/whoami">Get JSON</a>
-    </body>
-    </html>
-  `;
-
-  res.send(page);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/whoami", (req, res) => res.send(getInfo(req)));
 
-app.listen(process.env.PORT || 3000);
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  return res.json(
+    {
+      "ipaddress": req.ip,
+      "language": req.headers["accept-language"],
+      "software": req.headers["user-agent"]
+    }
+  );
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000 , function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
